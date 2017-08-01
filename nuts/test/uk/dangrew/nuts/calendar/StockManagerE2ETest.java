dcedf5b944bf629f8094fa9382bb120a1588a247
DanGrew <danielanthonygrew@gmail.com>
2017-08-01 20:15:29 +0100

    E2E TEST PLACEHOLDER:
    Added placeholder for future.

---
+++ nuts/test/uk/dangrew/nuts/calendar/StockManagerE2ETest.java
@@ -0,0 +1,11 @@
+package uk.dangrew.nuts.calendar;
+
+import org.junit.Test;
+
+public class StockManagerE2ETest {
+   
+   @Test public void untested() {
+      System.out.println( "WARNING: UNTESTED - " + getClass().getName() );
+   }//End Method
+
+}//End Class
