bb104bfe6f7eb7de0fa4bf2b622aeff0b1edc05a
DanGrew <danielanthonygrew@gmail.com>
2018-05-19 13:34:17 +0100

    DATE TIME TEXT BOX REPLACEMENT DIALOG:
    Replaced box in dialog for consistency and improvements.

--- nuts/src/uk/dangrew/nuts/graphics/common/UiDateTimeInputDialog.java
+++ nuts/src/uk/dangrew/nuts/graphics/common/UiDateTimeInputDialog.java
@@ -1,18 +1,13 @@
 package uk.dangrew.nuts.graphics.common;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
 import uk.dangrew.kode.javafx.style.JavaFxStyle;
 import uk.dangrew.kode.javafx.style.LabelBuilder;
 
@@ -25,30 +20,23 @@ public class UiDateTimeInputDialog extends Dialog< LocalDateTime >{
    private final ButtonType cancel;
    
    private final JavaFxStyle styling;
-   private final TextField dateField;
-   private final TextField timeField;
+   private final DateTimeTextBox timestampTextBox;
    
    public UiDateTimeInputDialog() {
       this.styling = new JavaFxStyle();
       this.setTitle( "Timestamp Input" );
       
-      this.dateField = new TextField();
-      this.dateField.setPromptText( "dd-mm-yyyy" );
-      this.dateField.textProperty().addListener( ( s, o, n ) -> validateDate() );
-      this.timeField = new TextField();
-      this.timeField.setPromptText( "hh:mm:ss" );
-      this.timeField.textProperty().addListener( ( s, o, n ) -> validateTime() );
+      this.timestampTextBox = new DateTimeTextBox();
       
       GridPane wrapper = new GridPane();
-      this.styling.configureConstraintsForEvenRows( wrapper, 3 );
+      this.styling.configureConstraintsForEvenRows( wrapper, 2 );
       this.styling.configureConstraintsForEvenColumns( wrapper, 1 );
       wrapper.add( 
             new LabelBuilder()
                .withText( "Please enter the date and time:" )
                .build(), 
       0, 0 );
-      wrapper.add( dateField, 0, 1 );
-      wrapper.add( timeField, 0, 2 );
+      wrapper.add( timestampTextBox, 0, 1 );
       wrapper.setPadding( new Insets( 10 ) );
       this.getDialogPane().setContent( wrapper );
       
@@ -57,70 +45,26 @@ public class UiDateTimeInputDialog extends Dialog< LocalDateTime >{
                cancel = ButtonType.CANCEL 
       );
       this.setResultConverter( this::resultFormatter );
-      
-      this.validateDate();
-      this.validateTime();
-      
       this.resetInputToNow();
    }//End Constructor
    
    public void resetInputToNow(){
-      LocalDateTime now = LocalDateTime.now();
-      dateField.setText( DATE_FORMATTER.format( now ) );
-      timeField.setText( TIME_FORMATTER.format( now ) );
+      timestampTextBox.resetInputToNow();
    }//End Method
    
    private LocalDateTime resultFormatter( ButtonType button ) {
       if ( button == confirm ) {
-         LocalDate date = validateDate();
-         LocalTime time = validateTime();
-         return LocalDateTime.of( date, time );
+         return timestampTextBox.getTextValue();
       }
       return null;
-   }//End Method
-   
-   private LocalDate validateDate(){
-      try { 
-         LocalDate parsed = LocalDate.parse( dateField.getText(), DATE_FORMATTER );
-         showValidation( dateField, true );
-         return parsed;
-      } catch ( DateTimeParseException exception ) {
-         showValidation( dateField, false );
-         return null;
-      }
-   }//End Method
-   
-   private LocalTime validateTime(){
-      try { 
-         LocalTime parsed = LocalTime.parse( timeField.getText(), TIME_FORMATTER );
-         showValidation( timeField, true );
-         return parsed;
-      } catch ( DateTimeParseException exception ) {
-         showValidation( timeField, false );
-         return null;
-      }
-   }//End Method
-   
-   private void showValidation( TextField field, boolean valid ) {
-      if ( valid ) {
-         field.setBorder( styling.borderFor( Color.GREEN ) );
-         getDialogPane().lookupButton( confirm ).setDisable( false );
-      } else {
-         field.setBorder( styling.borderFor( Color.RED ) );
-         getDialogPane().lookupButton( confirm ).setDisable( true );
-      }
    }//End Method
    
    public Optional< LocalDateTime > friendly_showAndWait(){
       return showAndWait();
    }//End Method
    
-   TextField dateField() {
-      return dateField;
-   }//End Method
-   
-   TextField timeField() {
-      return timeField;
+   DateTimeTextBox timestampBox() {
+      return timestampTextBox;
    }//End Method
    
 }//End Class
--- nuts/test/uk/dangrew/nuts/graphics/common/UiDateTimeInputDialogTest.java
+++ nuts/test/uk/dangrew/nuts/graphics/common/UiDateTimeInputDialogTest.java
@@ -18,6 +18,7 @@ import com.sun.javafx.application.PlatformImpl;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import uk.dangrew.kode.datetime.TimestampFormat;
 import uk.dangrew.kode.launch.TestApplication;
 
 public class UiDateTimeInputDialogTest {
@@ -40,59 +41,22 @@ public class UiDateTimeInputDialogTest {
    }//End Method
    
    private void assertThatInputIsInRangeOf( LocalDateTime timestamp, boolean insideRange ) {
-      LocalDate dateInBox = LocalDate.parse( systemUnderTest.dateField().getText(), UiDateTimeInputDialog.DATE_FORMATTER );
-      LocalTime timeInBox = LocalTime.parse( systemUnderTest.timeField().getText(), UiDateTimeInputDialog.TIME_FORMATTER );
-      LocalDateTime now = LocalDateTime.of( dateInBox, timeInBox );
+      LocalDateTime now = systemUnderTest.timestampBox().getTextValue();
       assertThat( now.isAfter( timestamp.minusMinutes( 5 ) ) && now.isBefore( timestamp.plusMinutes( 5 ) ), is( insideRange ) );
    }//End Method
    
    @Test public void shouldSetDateAndTimeToCurrent(){
-      systemUnderTest.dateField().setText( "01-01-1900" );
+      systemUnderTest.timestampBox().setText( "01-01-1900 05:06:07" );
       assertThatInputIsInRangeOf( LocalDateTime.now(), false );
       systemUnderTest.resetInputToNow();
       assertThatInputIsInRangeOf( LocalDateTime.now(), true );
-   }//End Method
-   
-   @Test public void shouldShowValidDate(){
-      assertThatBoxIsValid( systemUnderTest.dateField() );
-      systemUnderTest.dateField().setText( "flipper" );
-      assertThatBoxIsInvalid( systemUnderTest.dateField() );
-      systemUnderTest.dateField().setText( "01-01-1900" );
-      assertThatBoxIsValid( systemUnderTest.dateField() );
-      systemUnderTest.dateField().setText( "32-01-1900" );
-      assertThatBoxIsInvalid( systemUnderTest.dateField() );
-   }//End Method
-   
-   private void assertThatBoxIsValid( TextField box ) {
-      assertThat( box.getBorder().getStrokes().get( 0 ).getBottomStroke(), is( Color.GREEN ) );
-      assertThat( box.getBorder().getStrokes().get( 0 ).getTopStroke(), is( Color.GREEN ) );
-      assertThat( box.getBorder().getStrokes().get( 0 ).getRightStroke(), is( Color.GREEN ) );
-      assertThat( box.getBorder().getStrokes().get( 0 ).getLeftStroke(), is( Color.GREEN ) );
-   }//End Method
-   
-   private void assertThatBoxIsInvalid( TextField box ) {
-      assertThat( box.getBorder().getStrokes().get( 0 ).getBottomStroke(), is( Color.RED ) );
-      assertThat( box.getBorder().getStrokes().get( 0 ).getTopStroke(), is( Color.RED ) );
-      assertThat( box.getBorder().getStrokes().get( 0 ).getRightStroke(), is( Color.RED ) );
-      assertThat( box.getBorder().getStrokes().get( 0 ).getLeftStroke(), is( Color.RED ) );
-   }//End Method
-   
-   @Test public void shouldShowValidTime(){
-      assertThatBoxIsValid( systemUnderTest.timeField() );
-      systemUnderTest.timeField().setText( "flipper" );
-      assertThatBoxIsInvalid( systemUnderTest.timeField() );
-      systemUnderTest.timeField().setText( "16:06:06" );
-      assertThatBoxIsValid( systemUnderTest.timeField() );
-      systemUnderTest.timeField().setText( "13:61:00" );
-      assertThatBoxIsInvalid( systemUnderTest.timeField() );
    }//End Method
    
    @Test public void shouldConvertResult(){
       LocalDateTime now = LocalDateTime.now();
       now = now.minusNanos( now.getNano() );
       
-      systemUnderTest.dateField().setText( UiDateTimeInputDialog.DATE_FORMATTER.format( now ) );
-      systemUnderTest.timeField().setText( UiDateTimeInputDialog.TIME_FORMATTER.format( now ) );
+      systemUnderTest.timestampBox().setText( new TimestampFormat().toTimestampString( now ) );
       assertThat( systemUnderTest.getResultConverter().call( ButtonType.OK ), is( now ) );
    }//End Method
 
